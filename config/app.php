d90be3791c062235d7755e3e17517aceae9c6b19
muhamadhaspin <muhamadhaspin@gmail.com>
2023-08-02 14:59:35 +0700

    --cg : add Intervention Image package

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -184,6 +185,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'Image' => Intervention\Image\Facades\Image::class
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
